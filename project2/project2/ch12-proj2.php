1dacbe9d6e660b16f7484ba48d42d2500809b797
Mserp296 <113626184+Mserp296@users.noreply.github.com>
2026-02-06 19:12:57 +0000

    tutt

--- project2/project2/ch12-proj2.php
+++ project2/project2/ch12-proj2.php
@@ -27,53 +27,53 @@ function makeOptions(array $items): string {
 
 <main>
 <form class="form" id="mainForm" method="POST" action="art-process.php">
-   <fieldset class="form__panel">
-      <legend class="form__heading">Edit Art Work Details</legend>
+   <fieldset class="form_panel">
+      <legend class="form_heading">Edit Art Work Details</legend>
 
-        <p class="form__row">
+        <p class="form_row">
            <label>Title</label><br/>
-           <input type="text" name="title" class="form__input form__input--large" required />
+           <input type="text" name="title" class="form_input form_input-large" required />
        </p>
 
-       <p class="form__row">
+       <p class="form_row">
            <label>Description</label><br/>
-           <input type="text" name="description" class="form__input form__input--large" />
+           <input type="text" name="description" class="form_input form_input-large" />
        </p>
 
-       <p class="form__row">
+       <p class="form_row">
            <label>Genre</label><br/>
-           <select name="genre" class="form__input form__select">
+           <select name="genre" class="form_input form_select">
               <option value="">Choose genre</option>
               <?= makeOptions($genres) ?>
            </select>
        </p>
 
-       <p class="form__row">
+       <p class="form_row">
            <label>Subject</label><br/>
-           <select name="subject" class="form__input form__select">
+           <select name="subject" class="form_input form_select">
               <option value="">Choose subject</option>
               <?= makeOptions($subjects) ?>
            </select>
        </p>
 
-       <p class="form__row">
+       <p class="form_row">
            <label>Medium</label><br/>
-           <input type="text" name="medium" class="form__input form__input--medium" />
+           <input type="text" name="medium" class="form_input form_input-medium" />
        </p>
 
-       <p class="form__row">
+       <p class="form_row">
            <label>Year</label><br/>
-           <input type="text" name="year" class="form__input form__input--small" />
+           <input type="text" name="year" class="form_input form_input-small" />
        </p>
 
-       <p class="form__row">
+       <p class="form_row">
            <label>Museum</label><br/>
-           <input type="text" name="museum" class="form__input form__input--medium" />
+           <input type="text" name="museum" class="form_input form_input-medium" />
        </p>
 
-       <div class="form__box">
-          <input type="submit" class="form__btn" value="Submit">
-          <input type="reset" value="Clear Form" class="form__btn">
+       <div class="form_box">
+          <input type="submit" class="form_btn" value="Submit">
+          <input type="reset" value="Clear Form" class="form_btn">
        </div>
    </fieldset>
 </form>
